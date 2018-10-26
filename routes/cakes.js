641ebfda25217afb1a9f2f590b95e14b1b2e99b3
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-26 11:09:23 -0600

    added get one routes with some error handling

--- routes/cakes.js
+++ routes/cakes.js
@@ -6,7 +6,11 @@ router.get('/', (req, res, next) => {
   res.json({ cakes : cakes })
 });
 
-
+router.get('/:id',(req, res, next) => {
+  const id = req.params.id
+  let theCake = cakes.filter(cake => cake.id == id)
+  !Number(id) ? res.json({error: {status: 400, message: "Please enter a valid ID number"}}) : res.json({cake: theCake})
+})
 
 
 
--- routes/students.js
+++ routes/students.js
@@ -6,7 +6,11 @@ router.get('/', (req, res, next) => {
   res.json({ students : students })
 });
 
-
+router.get('/:id',(req, res, next) => {
+  const id = req.params.id
+  let theStudent = students.filter(student => student.id == id)
+  !Number(id) ? res.json({error: {status: 400, message: "Please enter a valid ID number"}}) : res.json({student: theStudent})
+})
 
 
 
