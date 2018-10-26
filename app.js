b29c5e46e475ae674ca3926da68f0f667d86b9a4
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-10-26 10:38:49 -0600

    added error handlers

--- app.js
+++ app.js
@@ -12,4 +12,17 @@ app.use(cors());
 app.use('/cakes', cakesRoutes);
 app.use('/students', studentsRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
+
+function notFound(err, req, res, next) {
+  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+};
+
+function errorHandler(err, req, res, next) {
+  console.error('NOPE, LOL', err)
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+};
+
 app.listen(port, () => console.log(`Your port is on ${port}`));
