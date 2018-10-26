6f4e4eab0d7830183bf9109ac1400157704c51fc
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-10-26 10:36:36 -0600

    add routes

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ const app = express();
 const bodyParser = ('body-parser');
 const cors = ('cors');
 const port = process.env.PORT || 3000;
+let cakesRoutes = ('./routes/cakes.js');
+let studentsRoutes = ('./routes/students.js');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/cakes', cakesRoutes);
+app.use('/students', studentsRoutes);
 
 app.listen(port, () => console.log(`Your port is on ${port}`));
